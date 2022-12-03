03eaa378bf6d2d7e17ec6864754cd3b8cc26df3e
Daniel D <dandoodle956@gmail.com>
2022-12-03 16:42:48 -0600

    Finished project

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+# Day 2 of 2022 Advent of Code
+# Daniel Dowdle
 def main():
     decrypt = {"A": "X",  # X
                "B": "Y",  # Y
@@ -24,6 +20,50 @@ def main():
     with open("input.txt") as file:
         lines = file.readlines()
 
+    partOne(lines, decrypt, scoring, x_beats_y)
+    partTwo(lines, scoring, decrypt)
+
+
+def partTwo(lines, scoring, decrypt):
+    # Winning takes in the opponents move
+    # and returns the move you should play to win
+    winning_move = {"A": "Y",  # Rock loses to paper            X Z
+                    "B": "Z",  # Paper loses to scissors        Y X
+                    "C": "X"  # scissors loses to rock          Z Y
+                    }
+
+    # Takes in the opponent's move
+    # and returns the move you should play in order to lose
+
+    losing_move = {"A": "Z",  # Rock beats scissors
+                   "B": "X",  # Paper beats rock
+                   "C": "Y"}  # Scissors beats paper
+
+    play_book = {"X": "lose",
+                 "Y": "draw",
+                 "Z": "win"}
+
+    score = 0
+    for line in lines:
+        line = line.strip("\n")
+        guide = line.split(" ")
+
+        opponent_move = guide[0]
+        my_outcome = guide[1]
+
+        if play_book[my_outcome] == "win":
+            score += scoring[winning_move[opponent_move]]
+            score += 6  # 6 points for a win
+        elif play_book[my_outcome] == "draw":
+            score += scoring[decrypt[opponent_move]]
+            score += 3
+        elif play_book[my_outcome] == "lose":
+            score += scoring[losing_move[opponent_move]]
+
+    print(score)
+
+
+def partOne(lines, decrypt, scoring, x_beats_y):
     total_score = 0
 
     for line in lines:
